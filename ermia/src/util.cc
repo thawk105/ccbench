6692242cf0437e1da15f4c2c852f4c121281e1c7
tanabe takayuki <tanabe@hpcs.cs.tsukuba.ac.jp>
2019-01-09 16:21:58 +0900

    ermia
    rm makeDB.cc makeProcedure.cc displayGlobal.cc
    They were integrated with util.cc.

--- ermia/src/util.cc
+++ ermia/src/util.cc
@@ -1,11 +1,24 @@
-//DISPLAY_GLOBAL_CC
+#include <atomic>
 #include <bitset>
+#include <cstdint>
 #include <iomanip>
+#include <iostream>
 #include <limits>
+#include <stdlib.h>
+
 #include "include/common.hpp"
 #include "include/debug.hpp"
+#include "include/procedure.hpp"
+#include "include/random.hpp"
+#include "include/tuple.hpp"
 
-using namespace std;
+bool chkClkSpan(uint64_t &start, uint64_t &stop, uint64_t threshold)
+{
+	uint64_t diff = 0;
+	diff = stop - start;
+	if (diff > threshold) return true;
+	else return false;
+}
 
 void
 displayDB()
@@ -98,3 +111,55 @@ displayAbortCounts()
 
 	cout << "Abort counts : " << sum << endl;
 }
+
+void
+makeDB()
+{
+	Tuple *tmp;
+	Version *verTmp;
+	Xoroshiro128Plus rnd;
+	rnd.init();
+
+	try {
+		if (posix_memalign((void**)&Table, 64, (TUPLE_NUM) * sizeof(Tuple)) != 0) ERR;
+		for (unsigned int i = 0; i < TUPLE_NUM; ++i) {
+			if (posix_memalign((void**)&Table[i].latest, 64, sizeof(Version)) != 0) ERR;
+		}
+	} catch (bad_alloc) {
+		ERR;
+	}
+
+	for (unsigned int i = 0; i < TUPLE_NUM; ++i) {
+		tmp = &Table[i];
+		tmp->key = i;
+		verTmp = tmp->latest.load(std::memory_order_acquire);
+		verTmp->cstamp = 0;
+		//verTmp->pstamp = 0;
+		//verTmp->sstamp = UINT64_MAX & ~(1);
+		verTmp->psstamp.pstamp = 0;
+		verTmp->psstamp.sstamp = UINT32_MAX & ~(1);
+		// cstamp, sstamp の最下位ビットは TID フラグ
+		// 1の時はTID, 0の時はstamp
+		verTmp->val = rnd.next() % TUPLE_NUM;
+		verTmp->prev = nullptr;
+		verTmp->committed_prev = nullptr;
+		verTmp->status.store(VersionStatus::committed, std::memory_order_release);
+		verTmp->readers.store(0, std::memory_order_release);
+	}
+}
+
+void
+makeProcedure(Procedure *pro, Xoroshiro128Plus &rnd)
+{
+	for (unsigned int i = 0; i < MAX_OPE; ++i) {
+		if ((rnd.next() % 10) < RRATIO) {
+			pro[i].ope = Ope::READ;
+		} else {
+			pro[i].ope = Ope::WRITE;
+		}
+		pro[i].key = rnd.next() % TUPLE_NUM;
+		pro[i].val = rnd.next() % TUPLE_NUM;
+	}
+}
+
+
