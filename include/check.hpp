9022c10a887ab5cbdbed6bf9585b8f708f9a8a4a
tanabe takayuki <tanabe@hpcs.cs.tsukuba.ac.jp>
2019-02-06 17:00:57 +0900

    silo move

---
+++ include/check.hpp
@@ -0,0 +1,17 @@
+
+#include <ctype.h>
+#include <string.h>
+
+#include <iostream>
+
+static bool
+chkInt(char *arg)
+{
+    for (uint i=0; i<strlen(arg); ++i) {
+        if (!isdigit(arg[i])) {
+          std::cout << std::string(arg) << " is not a number." << std::endl;
+			exit(0);
+        }
+    }
+	return true;
+}
