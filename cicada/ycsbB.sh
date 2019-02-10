acbfb8aed1872f45301a5eda09e2d84b12e7d6c0
tanabe takayuki <tanabe@hpcs.cs.tsukuba.ac.jp>
2019-02-10 14:39:34 +0900

    revise cicada/ycsbB.sh

--- cicada/ycsbB.sh
+++ cicada/ycsbB.sh
@@ -21,17 +21,17 @@ tuple=500
 result=result_cicada_ycsbB_tuple500_ar.dat
 rm $result
 echo "#worker thread, throughput, min, max" >> $result
-echo "#./cicada.exe $tuple $maxope thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $extime" >> $result
+echo "#./cicada.exe $tuple $maxope thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $gci $extime" >> $result
 
 thread=2
 sum=0
-echo "./cicada.exe $tuple $maxope $thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $extime"
+echo "./cicada.exe $tuple $maxope $thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $gci $extime"
 echo "$thread $epoch"
 max=0
 min=0	
 for ((i = 1; i <= epoch; ++i))
 do
-    tmp=`./cicada.exe $tuple $maxope $thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $extime`
+    tmp=`./cicada.exe $tuple $maxope $thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $gci $extime`
     sum=`echo "$sum + $tmp" | bc -l`
     echo "sum: $sum,   tmp: $tmp"
 
@@ -70,14 +70,14 @@ fi
 for ((thread=$inith; thread<=$enth; thread+=$inc))
 do
     sum=0
-	echo "./cicada.exe $tuple $maxope $thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $extime"
+	echo "./cicada.exe $tuple $maxope $thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $gci $extime"
 	echo "$thread $epoch"
  
  	max=0
 	min=0	
     for ((i = 1; i <= epoch; ++i))
     do
-        tmp=`./cicada.exe $tuple $maxope $thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $extime`
+        tmp=`./cicada.exe $tuple $maxope $thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $gci $extime`
         sum=`echo "$sum + $tmp" | bc -l`
         echo "sum: $sum,   tmp: $tmp"
 
@@ -108,17 +108,17 @@ tuple=500000
 result=result_cicada_ycsbB_tuple500k_ar.dat
 rm $result
 echo "#worker thread, throughput, min, max" >> $result
-echo "#./cicada.exe $tuple $maxope thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $extime" >> $result
+echo "#./cicada.exe $tuple $maxope thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $gci $extime" >> $result
 
 thread=2
 sum=0
-echo "./cicada.exe $tuple $maxope $thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $extime"
+echo "./cicada.exe $tuple $maxope $thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $gci $extime"
 echo "$thread $epoch"
 max=0
 min=0	
 for ((i = 1; i <= epoch; ++i))
 do
-    tmp=`./cicada.exe $tuple $maxope $thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $extime`
+    tmp=`./cicada.exe $tuple $maxope $thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $gci $extime`
     sum=`echo "$sum + $tmp" | bc -l`
     echo "sum: $sum,   tmp: $tmp"
 
@@ -157,14 +157,14 @@ fi
 for ((thread=$inith; thread<=$enth; thread+=$inc))
 do
     sum=0
-	echo "./cicada.exe $tuple $maxope $thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $extime"
+	echo "./cicada.exe $tuple $maxope $thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $gci $extime"
 	echo "$thread $epoch"
  
  	max=0
 	min=0	
     for ((i = 1; i <= epoch; ++i))
     do
-        tmp=`./cicada.exe $tuple $maxope $thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $extime`
+        tmp=`./cicada.exe $tuple $maxope $thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $gci $extime`
         sum=`echo "$sum + $tmp" | bc -l`
         echo "sum: $sum,   tmp: $tmp"
 
@@ -195,17 +195,17 @@ tuple=5000000
 result=result_cicada_ycsbB_tuple5m_ar.dat
 rm $result
 echo "#worker thread, throughput, min, max" >> $result
-echo "#./cicada.exe $tuple $maxope thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $extime" >> $result
+echo "#./cicada.exe $tuple $maxope thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $gci $extime" >> $result
 
 thread=2
 sum=0
-echo "./cicada.exe $tuple $maxope $thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $extime"
+echo "./cicada.exe $tuple $maxope $thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $gci $extime"
 echo "$thread $epoch"
 max=0
 min=0	
 for ((i = 1; i <= epoch; ++i))
 do
-    tmp=`./cicada.exe $tuple $maxope $thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $extime`
+    tmp=`./cicada.exe $tuple $maxope $thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $gci $extime`
     sum=`echo "$sum + $tmp" | bc -l`
     echo "sum: $sum,   tmp: $tmp"
 
@@ -244,14 +244,14 @@ fi
 for ((thread=$inith; thread<=$enth; thread+=$inc))
 do
     sum=0
-	echo "./cicada.exe $tuple $maxope $thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $extime"
+	echo "./cicada.exe $tuple $maxope $thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $gci $extime"
 	echo "$thread $epoch"
  
  	max=0
 	min=0	
     for ((i = 1; i <= epoch; ++i))
     do
-        tmp=`./cicada.exe $tuple $maxope $thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $extime`
+        tmp=`./cicada.exe $tuple $maxope $thread $rratio $skew $ycsb $wal $group_commit $cpu_mhz $io_time_ns $group_commit_timeout_us $lock_release $gci $extime`
         sum=`echo "$sum + $tmp" | bc -l`
         echo "sum: $sum,   tmp: $tmp"
 
