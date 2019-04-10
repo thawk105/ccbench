aa9d62065e9b7184b947d4f52a16eed174bde3d7
tanabe takayuki <tanabe@hpcs.cs.tsukuba.ac.jp>
2019-04-10 17:55:22 +0900

    add rdtscBench.cc

---
+++ instruction/rdtscBench.cc
@@ -0,0 +1,28 @@
+
+#include <iostream>
+
+#include "../include/tsc.hpp"
+
+#define LOOP 1000000
+
+using std::cout;
+using std::endl;
+
+int
+main()
+{
+  uint64_t start, stop;
+  start = rdtscp();
+  for (uint64_t i = 0; i < LOOP; ++i)
+    rdtsc();
+  stop = rdtscp();
+
+  cout << "rdtscBench[clocks] :\t" << (stop - start)/LOOP << endl;
+
+  start = rdtscp();
+  for (unsigned int i = 0; i < LOOP; ++i)
+    rdtscp();
+  stop = rdtscp();
+
+  cout << "rdtscpBench[clocks] :\t" << (stop - start)/LOOP << endl;
+}
