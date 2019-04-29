e48093fcb4fee92648ebb6eda31c6ba841be2ee5
tanabe takayuki <tanabe@hpcs.cs.tsukuba.ac.jp>
2019-04-29 16:43:18 +0900

    add test script

---
+++ instruction/masstree-simpleTest/script/test.sh
@@ -0,0 +1,37 @@
+
+result=result_masstree_insertion.dat
+rm $result
+epoch=3
+thread=1
+
+sumTH=0
+avgTH=0
+tmpTH=0
+for ((i = 1; i <= epoch; ++i))
+do
+  ../unit-mt $thread > out
+  tmpTH=`cat out`
+  sumTH=`echo "$sumTH + $tmpTH" | bc`
+done
+
+avgTH=`echo "$sumTH / $epoch" | bc`
+echo "$thread $avgTH" | tee -a $result
+
+for ((thread = 28; thread <= 224; thread+=28))
+do
+
+  sumTH=0
+  avgTH=0
+  tmpTH=0
+  for ((i = 1; i <= epoch; ++i))
+  do
+    ../unit-mt $thread > out
+    tmpTH=`cat out`
+    sumTH=`echo "$sumTH + $tmpTH" | bc`
+  done
+  
+  avgTH=`echo "$sumTH / $epoch" | bc`
+  echo "$thread $avgTH" | tee -a $result
+
+done
+
