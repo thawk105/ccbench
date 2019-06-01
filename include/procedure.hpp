cbf260389e4731603a3ccc6da8c417992ea952d1
tanabe takayuki <tanabe@hpcs.cs.tsukuba.ac.jp>
2019-06-01 14:29:38 +0900

    modify include/procedure.hpp.

--- include/procedure.hpp
+++ include/procedure.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
 enum class Ope : uint8_t {
   READ,
   WRITE,
@@ -7,13 +12,19 @@ enum class Ope : uint8_t {
 
 class Procedure {
 public:
-  Ope ope = Ope::READ;
+  Ope ope;
   uint64_t key;
 
+  Procedure() : ope(Ope::READ), key(0){}
+  Procedure(Ope ope_, uint64_t key_) : ope(ope_), key(key_) {}
+
   bool operator<(const Procedure& right) const {
-    /*if (this->key == right.key) {
-      if (this->ope == Ope::WRITE) return true;
-    }*/
+    if (this->key == right.key && this->ope == Ope::WRITE) {
+      return true;
+    }
+    /* キーが同値なら先に write ope を実行したい．read -> write よりも write -> read.*/
+
     return this->key < right.key;
   }
 };
+
