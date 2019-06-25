18e00ebc251e7bac5a04c654133ec541d3956a78
tanabe takayuki <tanabe@hpcs.cs.tsukuba.ac.jp>
2019-06-25 18:52:58 +0900

    update for measuring extra read and validation phase rate.

--- tictoc/script/ycsb-xval.sh
+++ tictoc/script/ycsb-xval.sh
@@ -45,6 +45,8 @@ do
   minTH=0
   minAR=0
   minCA=0
+  sumVPR=0
+  sumER=0
   for ((i = 1; i <= epoch; ++i))
   do
     if test $host = $dbs11 ; then
@@ -55,12 +57,16 @@ do
     fi
   
     tmpTH=`grep Throughput ./exp.txt | awk '{print $2}'`
-    tmpAR=`grep abortRate ./exp.txt | awk '{print $2}'`
+    tmpAR=`grep abort_rate ./exp.txt | awk '{print $2}'`
     tmpCA=`grep cache-misses ./ana.txt | awk '{print $4}'`
+    tmpER=`grep extra_reads ./exp.txt | awk '{print $2}'`
+    tmpVPR=`grep validation_phase_rate ./exp.txt | awk '{print $2}'`
     sumTH=`echo "$sumTH + $tmpTH" | bc`
     sumAR=`echo "scale=4; $sumAR + $tmpAR" | bc | xargs printf %.4f`
     sumCA=`echo "$sumCA + $tmpCA" | bc`
-    echo "tmpTH: $tmpTH, tmpAR: $tmpAR, tmpCA: $tmpCA"
+    sumER=`echo "$sumER + $tmpER" | bc`
+    sumVPR=`echo "scale=4; $sumVPR + $tmpVPR" | bc | xargs printf %.4f`
+    echo "tmpTH: $tmpTH, tmpAR: $tmpAR, tmpCA: $tmpCA, tmpER: $tmpER, tmpVPR: $tmpVPR"
   
     if test $i -eq 1 ; then
       maxTH=$tmpTH
@@ -101,10 +107,12 @@ do
   avgTH=`echo "$sumTH / $epoch" | bc`
   avgAR=`echo "scale=4; $sumAR / $epoch" | bc | xargs printf %.4f`
   avgCA=`echo "$sumCA / $epoch" | bc`
+  avgER=`echo "$sumER / $epoch" | bc`
+  avgVPR=`echo "scale=4; $sumVPR / $epoch" | bc | xargs printf %.4f`
   echo "sumTH: $sumTH, sumAR: $sumAR, sumCA: $sumCA"
-  echo "avgTH: $avgTH, avgAR: $avgAR, avgCA: $avgCA"
+  echo "avgTH: $avgTH, avgAR: $avgAR, avgCA: $avgCA, avgER: $avgER, avgVPR: $avgVPR"
   echo "maxTH: $maxTH, maxAR: $maxAR, maxCA: $maxCA"
   echo "minTH: $minTH, minAR: $minAR, minCA: $minCA"
   echo ""
-  echo "$val $avgTH $minTH $maxTH $avgAR $minAR $maxAR, $avgCA $minCA $maxCA" >> $result
+  echo "$val $avgTH $minTH $maxTH $avgAR $minAR $maxAR $avgCA $minCA $maxCA $avgER $avgVPR" >> $result
 done
