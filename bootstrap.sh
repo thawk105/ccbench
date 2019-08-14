c34c2e946132f80b189b513091a94be4acbaaddf
tanabe takayuki <thawk105@gmail.com>
2019-08-14 16:57:29 +0900

    bootstrap.sh, add update of LD_LIBRARY_PATH.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,25 +5,26 @@ git submodule update
 cd third_party/masstree
 ./bootstrap.sh
 ./configure
-make
+make -j
 
 # prepare for mimalloc
 cd ../mimalloc
 mkdir -p out/release
 cd out/release
 cmake ../..
-make
+make -j
 
 cd ../
 mkdir debug
 cd debug
 cmake -DCMAKE_BUILD_TYPE=Debug ../..
-make
+make -j
 
 cd ../
 mkdir secure
 cd secure
 cmake -DMI_SECURE=ON ../..
-make
+make -j
 
 cd ../../../../
+export $LD_LIBRARY_PATH=$PWD/third_party/mimalloc/out/release:$LD_LIBRARY_PATH
