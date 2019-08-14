7c014ff798b0abe3cf8be1b3c4a4196f65389f40
tanabe takayuki <tanabe@hpcs.cs.tsukuba.ac.jp>
2019-08-14 16:44:49 +0900

    Makefile を新しいディレクトリ構成に適応した．

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,29 @@
 git submodule init
 git submodule update
 
+# prepare for masstree
 cd third_party/masstree
 ./bootstrap.sh
 ./configure
-make -j 224
+make
 
+# prepare for mimalloc
 cd ../mimalloc
 mkdir -p out/release
 cd out/release
 cmake ../..
-make -j 224
+make
 
 cd ../
 mkdir debug
 cd debug
 cmake -DCMAKE_BUILD_TYPE=Debug ../..
-make -j 224
+make
 
 cd ../
 mkdir secure
 cd secure
 cmake -DMI_SECURE=ON ../..
-make -j 224
+make
 
 cd ../../../../
