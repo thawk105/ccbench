08c7bcde0cf78426b3eebda64d49d919f6a836db
tanabe takayuki <tanabe@hpcs.cs.tsukuba.ac.jp>
2019-08-14 15:08:30 +0900

    add transaction_table.hh

---
+++ si/include/transaction_table.hh
@@ -0,0 +1,17 @@
+#pragma once
+
+class TransactionTable {
+ public:
+  alignas(CACHE_LINE_SIZE) std::atomic<uint32_t> txid_;
+  std::atomic<uint32_t> lastcstamp_;
+
+  TransactionTable(uint32_t txid, uint32_t lastcstamp) {
+    this->txid_.store(txid, std::memory_order_relaxed);
+    this->lastcstamp_.store(lastcstamp, std::memory_order_relaxed);
+  }
+
+  void set(uint32_t txid, uint32_t lastcstamp) {
+    this->txid_.store(txid, std::memory_order_relaxed);
+    this->lastcstamp_.store(lastcstamp, std::memory_order_relaxed);
+  }
+};
