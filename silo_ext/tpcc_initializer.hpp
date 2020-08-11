5b1fdc9a545f5404369d59775d0e14d2cab9c6fc
albicilla <ojicyann@gmail.com>
2020-08-11 09:58:15 +0900

    Add files via upload
    add tpcc_initialize & tpcc_tables

---
+++ silo_ext/tpcc_initializer.hpp
@@ -0,0 +1,137 @@
+/*
+TPC-C
+http://www.tpc.org/tpc_documents_current_versions/pdf/tpc-c_v5.11.0.pdf
+
+1.3 Table Layouts
+
+*/
+
+#include "tpcc_tables.hpp"
+#include <random>
+
+namespace TPCC{
+namespace Initializer{
+
+    //random number generator
+    template <typename T>
+    T rangeRand(const T& min, const T& max) {
+    static thread_local std::mt19937 generator;
+    if constexpr (std::is_integral<T>{}) {
+        std::uniform_int_distribution<T> distribution(min, max);
+        return distribution(generator);
+    } else {
+        std::uniform_real_distribution<T> distribution(min, max);
+        return distribution(generator);
+    }
+    }
+
+
+    bool load(size_t warehouse){
+        std::time_t now = std::time(nullptr);
+
+        {
+            //CREATE Warehouses by single thread.
+            for(size_t w=0;w<warehouse;w++){
+                TPCC::Warehouse warehouse;
+                warehouse.W_ID = w;
+                warehouse.W_TAX = 1.5;
+                warehouse.W_YTD = 1000'000'000;
+
+                db.insert(warehouse.createKey(),&warehouse,sizeof(warehouse));
+
+            }
+
+        }
+        {
+            //CREATE  District
+            for(size_t w=0;w<warehouse;w++){
+                //10 districts per warehouse
+                for(size_t d=0;d<10;d++){
+                    TPCC::District district;
+                    district.D_ID=d;
+                    district.D_W_ID=w;
+                    district.D_NEXT_O_ID=0;
+                    district.D_TAX=1.5;
+                    district.D_YTD=1000'000'000;
+
+                    db.insert(warehouse.createKey(),&warehouse,sizeof(warehouse));
+
+                    // CREATE Customer. 3000 customers per a district.
+                    for(size_t c=0;c<3000;c++){
+                        TPCC::Custormer customer;
+                        customer.C_ID = c;
+                        customer.C_D_ID=d;
+                        customer.C_W_ID=w;
+                        strcpy(customer.C_LAST, "SUZUKI");
+                        customer.C_SINCE=now;
+                        strcpy(customer.C_CREDIT,"GC");
+                        customer.C_DISCOUNT=0.1;
+                        customer.C_BALANCE=-10.00;
+                        customer.C_YTD_PAYMENT  = 10.00;
+                        customer.C_PAYMENT_CNT=1;
+                        customer.C_PAYMENT_CNT  = 1;
+                        customer.C_DELIVERY_CNT = 0;
+
+                        db.insert(customer.createKey(),&customer,sizeof(customer));
+
+
+                        //CREATE History. 1 histories per customer.
+                        TPCC::History history;
+                        history.H_C_ID = c;
+                        history.H_C_D_ID = history.H_D_ID=d;
+                        history.H_C_W_ID = w;
+                        history.H_DATE=now;
+
+                        db.insert(history.createKey(),&history,sizeof(history));
+
+
+
+                        //CREATE Order. 1 order per customer.
+                        TPCC::Order order;
+                        order.O_ID=c;
+                        order.O_C_ID=c;
+                        order.O_D_ID=d;
+                        order.O_W_ID=w;
+                        order.O_ENTRY_D=now;
+                        
+                        db.insert(order.createKey(),&order,sizeof(order));
+
+                        //CREATE OrderLine. O_OL_CNT orderlines per order.
+                        for(size_t ol=0;ol<order.O_OL_CNT;ol++){
+                            TPCC::OrderLine order_line;
+                            order_line.OL_O_ID=order.O_ID;
+                            order_line.OL_D_ID=d;
+                            order_line.OL_NUMBER=ol;
+                            order_line.OL_I_ID=1;
+                            order_line.OL_SUPPLY_W_ID=w;
+                            order_line.OL_DELIVERY_D=now;
+                            order_line.OL_AMOUNT=0.0;
+
+                            db.insert(order_line.createKey(),&order_line,sizeof(order_line));
+
+                        }
+
+
+                        //CREATE NewOrder 900 rows.
+                        //900 rows in the NEW-ORDER table corresponding to the last
+                        //3000-2100=900
+                        if(2100<order.O_ID){
+                            TPCC::NewOrder new_order;
+                            new_order.NO_O_ID = order.O_ID;
+                            new_order.NO_D_ID = order.O_D_ID;
+                            new_order.NO_W_ID = order.O_W_ID;
+
+                            db.insert(new_order.createKey(),&new_order,sizeof(new_order));
+                        }                        
+
+                    }
+
+                }
+            }
+
+        }
+
+    }
+
+}
+}
---
+++ silo_ext/tpcc_tables.hpp
@@ -0,0 +1,225 @@
+
+/*
+TPC-C
+http://www.tpc.org/tpc_documents_current_versions/pdf/tpc-c_v5.11.0.pdf
+
+1.3 Table Layouts
+
+*/
+
+#include <cstdint>
+#include <string>
+#include <chrono>
+
+
+namespace TPCC {
+
+struct Warehouse{
+    constexpr const static char* kPrefix = "warehouse";
+
+    std::uint16_t W_ID; //2*W unique IDs
+    char W_NAME[10];
+    char W_STREET_1[20];
+    char W_STREET_2[20];
+    char W_CITY[20];
+    char W_STATE[2];
+    char W_ZIP[9];
+    double W_TAX;
+    double W_YTD;
+
+    //Primary Key: W_ID
+    static std::string CreateKey(size_t w_id){
+        return std::string(kPrefix + std::to_string(w_id));
+    }
+
+    std::string createKey(){return CreateKey(W_ID);}
+};
+
+
+
+struct District{
+    constexpr const static char* kPrefix = "district";
+
+    std::uint8_t D_ID; //20 unique IDs
+    std::uint16_t D_W_ID; //2*W unique IDs D_W_ID Foreign Key, references W_ID
+    char D_NAME[10];
+    char D_STREET_1[20];
+    char D_STREET_2[20];
+    char D_CITY[20];
+    char D_STATE[2];
+    char D_ZIP[9];
+    double D_TAX; //signed numeric(4,4) 
+    double D_YTD; //signed numeric(12,2)
+    std::uint32_t D_NEXT_O_ID; //10,000,000 unique IDs
+
+    //Primary Key: (D_W_ID, D_ID)
+    static std::string CreateKey(size_t w,size_t d){
+        return std::string(Warehouse::CreateKey(w)+kPrefix+std::to_string(d));
+    }
+
+    std::string createKey(){return CreateKey(D_W_ID,D_ID);}
+
+};
+
+struct Custormer{
+    constexpr const static char* kPrefix = "customer";
+
+    //(C_W_ID, C_D_ID) Foreign Key, references (D_W_ID, D_ID)
+    std::uint32_t C_ID; //96,000 unique IDs
+    std::uint8_t C_D_ID; //20 unique IDs
+    std::uint16_t C_W_ID; //2*W unique IDs
+    char C_FIRST[16];
+    char C_MIDDLE[2];
+    char C_LAST[16]; //the customer's last name
+    char C_STREET_1[20];
+    char C_STREET_2[20];
+    char C_CITY[20];
+    char C_STATE[2];
+    char C_ZIP[9];
+    char C_PHONE[16];
+    std::time_t C_SINCE;//date and time
+    char C_CREDIT[2];   //"GC"=good, "BC"=bad
+    double C_CREDIT_LIM;
+    double C_DISCOUNT;
+    double C_BALANCE;
+    double C_YTD_PAYMENT;
+    double C_PAYMENT_CNT;
+    double C_DELIVERY_CNT;
+    char C_DATA[50];
+
+    //Primary Key: (C_W_ID, C_D_ID, C_ID)
+    static std::string CreateKey(size_t w,size_t d,size_t c){
+        return std::string(District::CreateKey(w,d)+kPrefix+std::to_string(c));
+    }
+    std::string createKey(){return CreateKey(C_W_ID,C_D_ID,C_ID);}
+};
+
+struct History{
+
+    // Primary Key: none
+    // (H_C_W_ID, H_C_D_ID, H_C_ID) Foreign Key, references (C_W_ID, C_D_ID, C_ID)
+    // (H_W_ID, H_D_ID) Foreign Key, references (D_W_ID, D_ID)
+
+    uint32_t H_C_ID; //96,000 unique IDs
+    uint8_t H_C_D_ID;// 20 unique IDs
+    uint16_t H_C_W_ID; // 2*W unique IDs
+    uint8_t H_D_ID; //20 unique IDs
+    uint16_t H_W_ID; // 2*W unique IDs
+    std::time_t H_DATE; //date and time
+    double H_AMOUNT; //signed numeric(6, 2)
+    char H_DATA[24]; // variable text, size 24 Miscellaneous information
+
+
+};
+
+struct NewOrder{
+    constexpr const static char* kPrefix = "neworder";
+    //(NO_W_ID, NO_D_ID, NO_O_ID) Foreign Key, references (O_W_ID, O_D_ID, O_ID)
+    std::uint32_t NO_O_ID; //10,000,000 unique IDs
+    std::uint8_t NO_D_ID; //20 unique IDs
+    std::uint16_t NO_W_ID; //2*W unique IDs
+
+    //Primary Key: (NO_W_ID, NO_D_ID, NO_O_ID)
+    static std::string CreateKey(size_t w,size_t d,size_t o){
+        return std::string(kPrefix + std::to_string(w)+"-"+std::to_string(d)+"-"+std::to_string(o));
+    }
+
+    std::string createKey(){return CreateKey(NO_W_ID,NO_D_ID,NO_O_ID);}
+
+};
+
+struct Order{
+    constexpr const static char* kPrefix = "order";
+    //(O_W_ID, O_D_ID, O_C_ID) Foreign Key, references (C_W_ID, C_D_ID, C_ID)
+    std::uint32_t O_ID; //10,000,000 unique IDs
+    std::uint8_t O_D_ID; // 20 unique IDs
+    std::uint16_t O_W_ID; // 2*W unique IDs
+    std::uint32_t O_C_ID; //96,000 unique IDs
+    std::time_t O_ENTRY_D; //date and time
+    char O_CARRIER_ID[10]; // unique IDs, or null
+    double O_OL_CNT; //numeric(2) Count of Order-Lines
+    double O_ALL_LOCAL; //numeric(1)
+
+    //Primary Key: (O_W_ID, O_D_ID, O_ID)
+    static std::string CreateKey(size_t w,size_t d,size_t i){
+        return std::string(kPrefix + std::to_string(w)+"-"+std::to_string(d)+"-"+std::to_string(i));
+    }
+    std::string createKey(){return CreateKey(O_W_ID,O_D_ID,O_ID);}
+};
+
+struct OrderLine{
+    constexpr const static char* kPrefix = "orderline";
+
+    //(OL_W_ID, OL_D_ID, OL_O_ID) Foreign Key, references (O_W_ID, O_D_ID, O_ID)
+    //(OL_SUPPLY_W_ID, OL_I_ID) Foreign Key, references (S_W_ID, S_I_ID)
+    std::uint32_t OL_O_ID;// 10,000,000 unique IDs
+    std::uint8_t OL_D_ID;// 20 unique IDs
+    std::uint16_t OL_W_ID;// 2*W unique IDs
+    std::uint8_t OL_NUMBER;// 15 unique IDs
+    std::uint32_t OL_I_ID;// 200,000 unique IDs
+    std::uint16_t OL_SUPPLY_W_ID;// 2*W unique IDs
+    std::time_t OL_DELIVERY_D;// date and time, or null
+    double OL_QUANTITY;// numeric(2)
+    double OL_AMOUNT;// signed numeric(6, 2)
+    char OL_DIST_INFO[24];// fixed text, size 24
+
+    //Primary Key: (OL_W_ID, OL_D_ID, OL_O_ID, OL_NUMBER)
+    static std::string CreateKey(size_t w,size_t d,size_t o,size_t n){
+        return std::string(kPrefix+std::to_string(w)+"-"+std::to_string(d)+"-"+std::to_string(o)+"-"+std::to_string(n));
+    }
+    std::string createKey(){
+        return CreateKey(OL_W_ID,OL_D_ID,OL_O_ID,OL_NUMBER);
+    }
+};
+
+struct Item{
+    constexpr const static char* kPrefix = "item";
+
+    std::uint32_t I_ID; //200,000 unique IDs 100,000 items are populated
+    std::uint32_t I_IM_ID; //200,000 unique IDs Image ID associated to Item
+    char I_NAME[24]; //variable text, size 24
+    double I_PRICE; //numeric(5, 2)
+    char I_DATA[50]; //variable text, size 50 Brand information
+
+    //Primary Key: I_ID
+    static std::string CreateKey(size_t i){
+        return std::string(kPrefix+std::to_string(i));
+    }
+    std::string createKey(){
+        return CreateKey(I_ID);
+    }
+
+};
+
+struct Stock{
+    constexpr const static char* kPrefix = "stock";
+
+    //S_W_ID Foreign Key, references W_ID
+    //S_I_ID Foreign Key, references I_ID
+    std::uint16_t S_I_ID; //200,000 unique IDs 100,000 populated per warehouse
+    std::uint8_t S_W_ID; //2*W unique IDs
+    double S_QUANTITY; //signed numeric(4)
+    char S_DIST_01[24]; //fixed text, size 24
+    char S_DIST_02[24]; //fixed text, size 24
+    char S_DIST_03[24]; //fixed text, size 24
+    char S_DIST_04[24]; // fixed text, size 24
+    char S_DIST_05[24]; // fixed text, size 24
+    char S_DIST_06[24]; // fixed text, size 24
+    char S_DIST_07[24]; // fixed text, size 24
+    char S_DIST_08[24]; // fixed text, size 24
+    char S_DIST_09[24]; // fixed text, size 24
+    char S_DIST_10[24]; // fixed text, size 24
+    double S_YTD; // numeric(8)
+    double S_ORDER_CNT; // numeric(4)
+    double S_REMOTE_CNT; // numeric(4)
+    char S_DATA[50]; // variable text, size 50
+
+    // Primary Key: (S_W_ID, S_I_ID) composite.
+    static std::string CreateKey(size_t w, size_t i) {
+        return std::string(Warehouse::CreateKey(w)+kPrefix+std::to_string(i));
+    }
+
+    std::string createKey(){return CreateKey(S_W_ID, S_I_ID);}
+};
+
+}
