c109e8ff52eadb9b20503c812309d366ca0848b1
Masahiro TANAKA <masa16.tanaka@gmail.com>
2020-08-13 04:39:07 +0900

    copy result.cc util.cc atomic_tool.hh common.hh result.hh util.hh from silo directory

---
+++ silo_ext/include/common.hh
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <pthread.h>
+#include <atomic>
+#include <iostream>
+#include <queue>
+
+#include "tuple.hh"
+
+#include "../../include/cache_line_size.hh"
+#include "../../include/int64byte.hh"
+#include "../../include/masstree_wrapper.hh"
+
+#include "gflags/gflags.h"
+#include "glog/logging.h"
+
+#ifdef GLOBAL_VALUE_DEFINE
+#define GLOBAL
+alignas(CACHE_LINE_SIZE) GLOBAL uint64_t_64byte GlobalEpoch(1);
+#if MASSTREE_USE
+alignas(CACHE_LINE_SIZE) GLOBAL MasstreeWrapper<Tuple> MT;
+#endif
+#else
+#define GLOBAL extern
+alignas(CACHE_LINE_SIZE) GLOBAL uint64_t_64byte GlobalEpoch;
+#if MASSTREE_USE
+alignas(CACHE_LINE_SIZE) GLOBAL MasstreeWrapper<Tuple> MT;
+#endif
+#endif
+
+#ifdef GLOBAL_VALUE_DEFINE
+DEFINE_uint64(clocks_per_us, 2100,
+              "CPU_MHz. Use this info for measuring time.");
+DEFINE_uint64(epoch_time, 40, "Epoch interval[msec].");
+DEFINE_uint64(extime, 3, "Execution time[sec].");
+DEFINE_uint64(max_ope, 10,
+              "Total number of operations per single transaction.");
+DEFINE_bool(rmw, false,
+            "True means read modify write, false means blind write.");
+DEFINE_uint64(rratio, 50, "read ratio of single transaction.");
+DEFINE_uint64(thread_num, 10, "Total number of worker threads.");
+DEFINE_uint64(tuple_num, 1000000, "Total number of records.");
+DEFINE_bool(ycsb, true,
+            "True uses zipf_skew, false uses faster random generator.");
+DEFINE_double(zipf_skew, 0, "zipf skew. 0 ~ 0.999...");
+#else
+DECLARE_uint64(clocks_per_us);
+DECLARE_uint64(epoch_time);
+DECLARE_uint64(extime);
+DECLARE_uint64(max_ope);
+DECLARE_bool(rmw);
+DECLARE_uint64(rratio);
+DECLARE_uint64(thread_num);
+DECLARE_uint64(tuple_num);
+DECLARE_bool(ycsb);
+DECLARE_double(zipf_skew);
+#endif
+
+alignas(CACHE_LINE_SIZE) GLOBAL uint64_t_64byte *ThLocalEpoch;
+alignas(CACHE_LINE_SIZE) GLOBAL uint64_t_64byte *CTIDW;
+
+alignas(CACHE_LINE_SIZE) GLOBAL Tuple *Table;
---
+++ silo_ext/include/result.hh
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <vector>
+
+#include "../../include/result.hh"
+
+extern std::vector <Result> SiloResult;
+
+extern void initResult();
