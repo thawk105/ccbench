7b4329211a29caf3569403604f812d26e310d26d
Takayuki Tanabe <thawk105@gmail.com>
2020-08-12 13:45:08 +0900

    update dev_test and unit_test

--- silo_ext/test/unit_test.cpp
+++ silo_ext/test/unit_test.cpp
@@ -28,4 +28,26 @@ TEST_F(unit_test, direct_db_access) { // NOLINT
   ASSERT_EQ(got_rec_ptr->get_tuple().get_key(), std::string_view(a));
 }
 
+TEST_F(unit_test, tx_session_test) { // NOLINT
+  Token token{};
+  ASSERT_EQ(enter(token), Status::OK);
+  ASSERT_EQ(leave(token), Status::OK);
+}
+
+TEST_F(unit_test, tx_insert_test) { // NOLINT
+  Token token{};
+  std::string a{"a"};
+  std::string b{"b"};
+  ASSERT_EQ(enter(token), Status::OK);
+  ASSERT_EQ(insert(token, Storage::CUSTOMER, a, b), Status::OK);
+  Tuple *ret_tuple_ptr;
+  ASSERT_EQ(search_key(token, Storage::CUSTOMER, a, &ret_tuple_ptr), Status::WARN_READ_FROM_OWN_OPERATION);
+  ASSERT_EQ(ret_tuple_ptr->get_val(), std::string_view(b));
+  ASSERT_EQ(commit(token), Status::OK);
+  ASSERT_EQ(search_key(token, Storage::CUSTOMER, a, &ret_tuple_ptr), Status::OK);
+  ASSERT_EQ(ret_tuple_ptr->get_val(), std::string_view(b));
+  ASSERT_EQ(commit(token), Status::OK);
+  ASSERT_EQ(leave(token), Status::OK);
+}
+
 } // namespace ccbench::testing
