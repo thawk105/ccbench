465bd859f6cd5badc1ec68fe4f8892c5f026dc51
albicilla <t.albicilla@gmail.com>
2020-08-13 00:11:38 +0900

    checkout river.cpp

--- silo_ext/river.cpp
+++ silo_ext/river.cpp
@@ -12,39 +12,18 @@ bool init_table_warehouse(size_t nwh){
 	
 	std::time_t now = std::time(nullptr);
 	{
-
-        std::string mem;
-        
 		//CREATE Warehouses by single thread.
 		for (size_t w = 0; w < nwh; w++) {
 			TPCC::Warehouse wh;
 			wh.W_ID = w;
 			wh.W_TAX = 1.5;
 			wh.W_YTD = 1000'000'000;
-
-            char* ptr=reinterpret_cast<char*>(&wh);
-            size_t siz=sizeof(TPCC::Warehouse);
-            //std::cout<<siz<<std::endl;
-            insert(token, Storage::WAREHOUSE, wh.createKey(), {ptr,siz});
-            mem=wh.createKey();
-        }
-
-        commit(token);
-
-        
-        Tuple *ret;
-        if(search_key(token,Storage::WAREHOUSE,mem,&ret)==Status::WARN_NOT_FOUND){
-            std::cout<<"not found"<<std::endl;
-        }else{
-            std::cout<<"ok"<<std::endl;
-        }
-        commit(token);
-        
-        leave(token);
-        fin();
-        
-       
+			insert(token, Storage::WAREHOUSE, wh.CreateKey(w), (char *)&wh);
+		}
 	}
+	commit(token);
+	leave(token);
+	fin();
 }
 
 void
@@ -72,12 +51,9 @@ int
 //tpcc_txn_man::run_new_order(tpcc_query * query)
 main(void)
 {
-
-    //	makeDBforTPCC();
-	//makedb_tpcc();
-	//const int nwh = 224;
-	init_table_warehouse(1);
+	makedb_tpcc();
+	const int nwh = 224;
+	init_table_warehouse(nwh);
 	
-    
 	return 0;
 }
