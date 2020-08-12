60117ff8ce8b5fbe94696d406e0de0330f694f00
Takayuki Tanabe <thawk105@gmail.com>
2020-08-12 17:38:40 +0900

    update build script

--- silo_ext/build_script.sh
+++ silo_ext/build_script.sh
@@ -1,5 +1,8 @@
+apt  update -y && apt install -y libgflags-dev libgoogle-glog-dev libboost-filesystem-dev
+
 cd ../
 ./bootstrap.sh
+./bootstrap_apt.sh
 ./bootstrap_mimalloc.sh
 ./bootstrap_googletest.sh
 
