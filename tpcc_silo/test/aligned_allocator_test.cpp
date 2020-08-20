9ef7acb4a7b8ac7ae2765216687db39807c00516
Takayuki Tanabe <thawk105@gmail.com>
2020-08-20 22:54:08 +0900

    improve allocator test.

--- tpcc_silo/test/aligned_allocator_test.cpp
+++ tpcc_silo/test/aligned_allocator_test.cpp
@@ -9,15 +9,18 @@ namespace ccbench::testing {
 
 using namespace ccbench;
 
-class aligned_allocator_test : public ::testing::Test {};
+class aligned_allocator_test : public ::testing::Test {
+};
 
-TEST_F(aligned_allocator_test, storage_test) { // NOLINT
+TEST_F(aligned_allocator_test, alignment_test) { // NOLINT
   ASSERT_EQ(true, true);
-  /**
-   * TODO
-   * 1. std::string a = new std::string(?, ccbench::aligned_allocator<std::string, b>)
-   * 2. ASSERT_EQ(alignof(a.data()), b)
-   */
+  constexpr std::size_t align = 64;
+  auto *str = new std::basic_string<char, std::char_traits<char>, aligned_allocator<std::string, align>>();
+  std::cout << reinterpret_cast<uintptr_t>(reinterpret_cast<void *>(&str)) << std::endl;
+  std::cout << reinterpret_cast<uintptr_t>(reinterpret_cast<void *>(&str)) % align << std::endl;
+  std::cout << reinterpret_cast<uintptr_t>(reinterpret_cast<void *>(str->data())) << std::endl;
+  std::cout << reinterpret_cast<uintptr_t>(reinterpret_cast<void *>(str->data())) % align << std::endl;
+  delete str;
 }
 
 } // namespace ccbench::testing
