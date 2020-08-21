c8d8e0e2eba35c48f134372004ae46481e2faee9
Takayuki Tanabe <thawk105@gmail.com>
2020-08-21 10:32:49 +0900

    update bootstrap_mimalloc.sh

--- bootstrap_mimalloc.sh
+++ bootstrap_mimalloc.sh
@@ -3,22 +3,22 @@ cd third_party/mimalloc
 mkdir -p out/release
 cd out/release
 cmake ../..
-make -j
+make clean all -j
 
 cd ../
 mkdir debug
 cd debug
 cmake -DCMAKE_BUILD_TYPE=Debug ../..
-make -j
+make clean all -j
 
 cd ../
 mkdir secure
 cd secure
 cmake -DMI_SECURE=ON ../..
-make -j
+make clean all -j
 
 # prepare for tbb
 cd ../../../tbb
-make -j
+make clean all -j
 
 cd ../../
