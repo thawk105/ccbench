f3c6a559ec716a72e2586538ad2e3afdd329ca7f
Takayuki Tanabe <thawk105@gmail.com>
2020-08-23 20:30:35 +0900

    add tpcc_initializer_test.ccp

---
+++ tpcc_silo/test/tpcc_initializer_test.cpp
@@ -0,0 +1,28 @@
+//
+// Created by thawk on 2020/08/12.
+//
+
+#define GLOBAL_VALUE_DEFINE
+#include "common.hh"
+#include "gtest/gtest.h"
+#include "tpcc/tpcc_initializer.hpp"
+
+namespace ccbench::testing {
+
+using namespace ccbench;
+using namespace TPCC::Initializer;
+
+class tpcc_initializer_test : public ::testing::Test {
+};
+
+TEST_F(tpcc_initializer_test, db_insert_test) { // NOLINT
+  std::string key{"key"};
+  std::string val{"val"};
+  db_insert(Storage::STOCK, key, val, alignof(std::string));
+  void *ret_ptr = kohler_masstree::find_record(Storage::STOCK, key);
+  ASSERT_NE(ret_ptr, nullptr);
+  std::string_view ret_val_view = static_cast<Record *>(ret_ptr)->get_tuple().get_val();
+  ASSERT_EQ(memcmp(val.data(), ret_val_view.data(), val.size()), 0);
+}
+
+} // namespace ccbench::testing
