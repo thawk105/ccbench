60f69c347d132f5b4a4702ff726c760d87f99893
Hideyuki Kawashima <river@sfc.keio.ac.jp>
2020-08-25 16:11:47 +0900

    remove dbx1000 codes

--- tpcc_silo/tpcc/neworder.cpp
+++ tpcc_silo/tpcc/neworder.cpp
@@ -6,15 +6,6 @@ using namespace ccbench;
 
 namespace TPCC {
 
-#ifdef DBx1000
-#if TPCC_SMALL
-const uint32_t g_max_items = 10000;
-const int32_t g_cust_per_dist = 2000;
-#else
-const uint32_t g_max_items = 100000;
-const uint32_t g_cust_per_dist = 3000;
-#endif
-
 //#define DIST_PER_WARE (10)
 
 // Helper Functions
@@ -77,37 +68,6 @@ run_new_order(TPCC::query::NewOrder *query, Token &token) {
   [[maybe_unused]] char *c_last = cust->C_LAST;
   [[maybe_unused]] char *c_credit = cust->C_CREDIT;
 
-#ifdef DBx1000
-  key = w_id;
-  index = _wl->i_warehouse;
-  item = index_read(index, key, wh_to_part(w_id));
-  assert(item != NULL);
-  row_t * r_wh = ((row_t *)item->location);
-  row_t * r_wh_local = get_row(r_wh, RD);
-  if (r_wh_local == NULL) {
-    return finish(Abort);
-  }
-
-
-  double w_tax;
-  r_wh_local->get_value(W_TAX, w_tax);
-  key = custKey(c_id, d_id, w_id);
-  index = _wl->i_customer_id;
-  item = index_read(index, key, wh_to_part(w_id));
-  assert(item != NULL);
-  row_t * r_cust = (row_t *) item->location;
-  row_t * r_cust_local = get_row(r_cust, RD);
-  if (r_cust_local == NULL) {
-    return finish(Abort);
-  }
-  uint64_t c_discount;
-  //char * c_last;
-  //char * c_credit;
-  r_cust_local->get_value(C_DISCOUNT, c_discount);
-  //c_last = r_cust_local->get_value(C_LAST);
-  //c_credit = r_cust_local->get_value(C_CREDIT);
-#endif
-
   /*==================================================+
     EXEC SQL SELECT d_next_o_id, d_tax
     INTO :d_next_o_id, :d_tax
@@ -139,28 +99,10 @@ run_new_order(TPCC::query::NewOrder *query, Token &token) {
     return false;
   }
 
-#ifdef DBx1000
-  key = distKey(d_id, w_id);
-  item = index_read(_wl->i_district, key, wh_to_part(w_id));
-  assert(item != NULL);
-  row_t * r_dist = ((row_t *)item->location);
-  row_t * r_dist_local = get_row(r_dist, WR);
-  if (r_dist_local == NULL) {
-    return finish(Abort);
-  }
-  //double d_tax;
-  int64_t o_id;
-  //d_tax = *(double *) r_dist_local->get_value(D_TAX);
-  o_id = *(int64_t *) r_dist_local->get_value(D_NEXT_O_ID);
-  o_id ++;
-  r_dist_local->set_value(D_NEXT_O_ID, o_id);
-#endif
-
-
-  /*========================================================================================+
+  /*=========================================+
     EXEC SQL INSERT INTO ORDERS (o_id, o_d_id, o_w_id, o_c_id, o_entry_d, o_ol_cnt, o_all_local)
     VALUES (:o_id, :d_id, :w_id, :c_id, :datetime, :o_ol_cnt, :o_all_local);
-    +========================================================================================*/
+    +=======================================*/
   HeapObject order_obj;
   order_obj.allocate<TPCC::Order>();
   TPCC::Order& order = order_obj.ref();
@@ -180,21 +122,6 @@ run_new_order(TPCC::query::NewOrder *query, Token &token) {
     return false;
   }
 
-#ifdef DBx1000
-  row_t * r_order;
-  uint64_t row_id;
-  _wl->t_order->get_new_row(r_order, 0, row_id);
-  r_order->set_value(O_ID, o_id);
-  r_order->set_value(O_C_ID, c_id);
-  r_order->set_value(O_D_ID, d_id);
-  r_order->set_value(O_W_ID, w_id);
-  r_order->set_value(O_ENTRY_D, o_entry_d);
-  r_order->set_value(O_OL_CNT, ol_cnt);
-  int64_t all_local = (remote? 0 : 1);
-  r_order->set_value(O_ALL_LOCAL, all_local);
-  insert_row(r_order, _wl->t_order);
-#endif
-
   /*=======================================================+
     EXEC SQL INSERT INTO NEW_ORDER (no_o_id, no_d_id, no_w_id)
     VALUES (:o_id, :d_id, :w_id);
@@ -213,16 +140,6 @@ run_new_order(TPCC::query::NewOrder *query, Token &token) {
     abort(token);
     return false;
   }
-
-
-#ifdef DBx1000
-  row_t * r_no;
-  _wl->t_neworder->get_new_row(r_no, 0, row_id);
-  r_no->set_value(NO_O_ID, o_id);
-  r_no->set_value(NO_D_ID, d_id);
-  r_no->set_value(NO_W_ID, w_id);
-  insert_row(r_no, _wl->t_neworder);
-#endif
 
   for (std::uint32_t ol_number = 0; ol_number < ol_cnt; ++ol_number) {
     /*===========================================+
@@ -254,26 +171,6 @@ run_new_order(TPCC::query::NewOrder *query, Token &token) {
     double i_price = item->I_PRICE;
     [[maybe_unused]] char *i_name = item->I_NAME;
     [[maybe_unused]] char *i_data = item->I_DATA;
-
-#ifdef DBx1000
-    uint64_t ol_i_id = query->items[ol_number].ol_i_id;
-    uint64_t ol_supply_w_id = query->items[ol_number].ol_supply_w_id;
-    uint64_t ol_quantity = query->items[ol_number].ol_quantity;
-    key = ol_i_id;
-    item = index_read(_wl->i_item, key, 0);
-    assert(item != NULL);
-    row_t * r_item = ((row_t *)item->location);
-    row_t * r_item_local = get_row(r_item, RD);
-    if (r_item_local == NULL) {
-      return finish(Abort);
-    }
-    int64_t i_price;
-    char * i_name;
-    char * i_data;
-    r_item_local->get_value(I_PRICE, i_price);
-    i_name = r_item_local->get_value(I_NAME);
-    i_data = r_item_local->get_value(I_DATA);
-#endif
 
     /*===================================================================+
       EXEC SQL SELECT s_quantity, s_data,
@@ -349,48 +246,6 @@ run_new_order(TPCC::query::NewOrder *query, Token &token) {
       return false;
     }
 
-#ifdef DBx1000
-    uint64_t stock_key = stockKey(ol_i_id, ol_supply_w_id);
-    INDEX * stock_index = _wl->i_stock;
-    itemid_t * stock_item;
-    index_read(stock_index, stock_key, wh_to_part(ol_supply_w_id), stock_item);
-    assert(item != NULL);
-    row_t * r_stock = ((row_t *)stock_item->location);
-    row_t * r_stock_local = get_row(r_stock, WR);
-    if (r_stock_local == NULL) {
-      return finish(Abort);
-    }
-
-    // XXX s_dist_xx are not retrieved.
-    UInt64 s_quantity;
-    int64_t s_remote_cnt;
-    s_quantity = *(int64_t *)r_stock_local->get_value(S_QUANTITY);
-
-#if !TPCC_SMALL
-    int64_t s_ytd;
-    int64_t s_order_cnt;
-    r_stock_local->get_value(S_YTD, s_ytd);
-    r_stock_local->set_value(S_YTD, s_ytd + ol_quantity);
-    r_stock_local->get_value(S_ORDER_CNT, s_order_cnt);
-    r_stock_local->set_value(S_ORDER_CNT, s_order_cnt + 1);
-    s_data = r_stock_local->get_value(S_DATA);
-#endif
-
-    if (remote) {
-      s_remote_cnt = *(int64_t*)r_stock_local->get_value(S_REMOTE_CNT);
-      s_remote_cnt ++;
-      r_stock_local->set_value(S_REMOTE_CNT, &s_remote_cnt);
-    }
-    uint64_t quantity;
-    if (s_quantity > ol_quantity + 10) {
-      quantity = s_quantity - ol_quantity;
-    } else {
-      quantity = s_quantity - ol_quantity + 91;
-    }
-    r_stock_local->set_value(S_QUANTITY, &quantity);
-
-#endif /* DBx1000 */
-
     /*====================================================+
       EXEC SQL INSERT INTO order_line(ol_o_id, ol_d_id, ol_w_id, ol_number,	ol_i_id, ol_supply_w_id,
       ol_quantity, ol_amount, ol_dist_info)
@@ -436,9 +291,6 @@ run_new_order(TPCC::query::NewOrder *query, Token &token) {
     copy_cstr(orderline.OL_DIST_INFO, pick_sdist(), sizeof(orderline.OL_DIST_INFO));
 #endif
 
-#if !TPCC_SMALL
-    // distinfo?
-#endif
     // The number of keys is enough? It is different from DBx1000
     SimpleKey<8> ol_key;
     TPCC::OrderLine::CreateKey(orderline.OL_W_ID, orderline.OL_D_ID, orderline.OL_O_ID,
@@ -448,29 +300,7 @@ run_new_order(TPCC::query::NewOrder *query, Token &token) {
       abort(token);
       return false;
     }
-#ifdef DBx1000
-    row_t * r_ol;
-    uint64_t row_id;
-    _wl->t_orderline->get_new_row(r_ol, 0, row_id);
-    r_ol->set_value(OL_O_ID, &o_id);
-    r_ol->set_value(OL_D_ID, &d_id);
-    r_ol->set_value(OL_W_ID, &w_id);
-    r_ol->set_value(OL_NUMBER, &ol_number);
-    r_ol->set_value(OL_I_ID, &ol_i_id);
 
-#if !TPCC_SMALL
-    int w_tax=1, d_tax=1;
-    int64_t ol_amount = ol_quantity * i_price * (1 + w_tax + d_tax) * (1 - c_discount);
-    r_ol->set_value(OL_SUPPLY_W_ID, &ol_supply_w_id);
-    r_ol->set_value(OL_QUANTITY, &ol_quantity);
-    r_ol->set_value(OL_AMOUNT, &ol_amount);
-#endif
-    insert_row(r_ol, _wl->t_orderline);
-  }
-  assert( rc == RCOK );
-
-  //return finish(rc);
-#endif // DBx1000
   } // end of ol loop
   if (commit(token) == Status::OK) {
     return true;
