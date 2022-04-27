7ea95aaa0405b5155c024aaf173dfa0dcac7879e
Takayuki Tanabe <thawk105@gmail.com>
2022-04-27 14:32:30 +0900

    update ss2pl's test

--- ss2pl/test/make_db_test.cpp
+++ ss2pl/test/make_db_test.cpp
@@ -1,8 +1,11 @@
 
+#include <mutex>
+
 #define GLOBAL_VALUE_DEFINE
 
-#include "../include/common.hh"
 #include "../../include/backoff.hh"
+#include "../include/common.hh"
+#include "../include/util.hh"
 
 #include "glog/logging.h"
 #include "gtest/gtest.h"
@@ -11,13 +14,27 @@ namespace ccbench::testing {
 
 class make_db_test : public ::testing::Test {
 public:
-    void SetUp() override {}
+    static void call_once_f() {
+        google::InitGoogleLogging("make_db_test_log");
+        FLAGS_stderrthreshold = 0;
+    }
+
+    void SetUp() override { std::call_once(init_, call_once_f); }
 
     void TearDown() override {}
+
+private:
+    static inline std::once_flag init_; // NOLINT
 };
 
 TEST_F(make_db_test, simple) { // NOLINT
-    ASSERT_EQ(true, true);
+    makeDB();
+    // verify effect makeDb
+    for (std::uint64_t i = 0; i < FLAGS_tuple_num; ++i) {
+        ASSERT_EQ(Table[i].val_[0], 'a');
+        ASSERT_EQ(Table[i].val_[1], '\0');
+        ASSERT_EQ(Table[i].lock_.counter.load(std::memory_order_acquire), 0);
+    }
 }
 
 } // namespace ccbench::testing
